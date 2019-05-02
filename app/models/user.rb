b62fe21fd3ca80d158a3d43e1e5b8a96df7ebb2a
Andrew Waller <andrewhwaller@gmail.com>
2019-05-02 02:04:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+
 class User < ActiveRecord::Base
-  has_many :tweets
+  has_many  :tweets
+
 
   has_secure_password
 
@@ -10,4 +12,5 @@ class User < ActiveRecord::Base
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
   end
+
 end
