ffb6efd62d251d631c24b92f9f04ba67b1972add
Andrew Waller <andrewhwaller@gmail.com>
2019-05-02 02:08:55 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/tweets"
+      redirect '/tweets'
     else
       redirect '/signup'
     end
