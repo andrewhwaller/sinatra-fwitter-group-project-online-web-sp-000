6a7758b68d650fcaf2ec5d71f6273dc5514e2c19
Andrew Waller <andrewhwaller@gmail.com>
2019-05-02 00:24:37 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,11 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/new' do
-    erb :'/tweets/new'
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
   end
 
   post '/tweets' do
