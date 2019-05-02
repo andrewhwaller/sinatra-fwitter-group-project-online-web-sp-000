9e0ae51b74d4f5ae42f596f57e2714370e48f376
Andrew Waller <andrewhwaller@gmail.com>
2019-05-02 02:08:49 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect "/tweets"
     else
-      redirect to '/signup'
+      redirect '/signup'
     end
   end
 
